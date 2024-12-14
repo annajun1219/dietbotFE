a724b236c0352edb7632ccf0d4596f1e071ac4c7
annajun1219 <annajun@sookmyung.ac.kr>
2024-12-14 19:00:48 +0900

    최종

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./apps/mainpage";
 import ChatBotPage from "./apps/chatbot"; 
 import MyPage from "./apps/mypage";
+import InputPage from "./apps/inputpage";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/chatbot" element={<ChatBotPage />} /> {/* ChatBot 경로 추가 */}
-        <Route path="/mypage" element={<MyPage />} />
-      </Routes>
+      <Header />
+      <div className="pt-20">
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/chatbot" element={<ChatBotPage />} /> {/* ChatBot 경로 추가 */}
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/inputpage" element={<InputPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
